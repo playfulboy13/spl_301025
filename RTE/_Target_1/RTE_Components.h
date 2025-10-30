bc4cfbd0b43d430c9462b13826a6380e6c926884
HA QUAN <quan.kieu.2013.dt@gmail.com>
2025-10-30 09:30:41 +0700

    test spl 1

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,77 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'spl_301025' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/*  ARM.FreeRTOS::RTOS:Config:CMSIS RTOS2:11.1.0 */
+#define RTE_RTOS_FreeRTOS_CONFIG_RTOS2  /* RTOS FreeRTOS Config for CMSIS RTOS2 API */
+/*  ARM.FreeRTOS::RTOS:Core:Cortex-M:11.1.0 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/*  ARM.FreeRTOS::RTOS:Coroutines:11.1.0 */
+#define RTE_RTOS_FreeRTOS_COROUTINE     /* RTOS FreeRTOS Co-routines */
+/*  ARM.FreeRTOS::RTOS:Event Groups:11.1.0 */
+#define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
+/*  ARM.FreeRTOS::RTOS:Heap:Heap_4:11.1.0 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/*  ARM.FreeRTOS::RTOS:Message Buffer:11.1.0 */
+#define RTE_RTOS_FreeRTOS_MESSAGE_BUFFER /* RTOS FreeRTOS Message Buffers */
+/*  ARM.FreeRTOS::RTOS:Stream Buffer:11.1.0 */
+#define RTE_RTOS_FreeRTOS_STREAM_BUFFER /* RTOS FreeRTOS Stream Buffers */
+/*  ARM.FreeRTOS::RTOS:Timers:11.1.0 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/*  ARM::CMSIS:RTOS2:FreeRTOS:Cortex-M:11.1.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
+/*  Keil.MDK-Plus::USB:CORE:Release:6.16.1 */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Release            /* USB Core Release Version */
+/*  Keil::Device:StdPeriph Drivers:ADC:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_ADC
+/*  Keil::Device:StdPeriph Drivers:BKP:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_BKP
+/*  Keil::Device:StdPeriph Drivers:CRC:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_CRC
+/*  Keil::Device:StdPeriph Drivers:DBGMCU:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_DBGMCU
+/*  Keil::Device:StdPeriph Drivers:DMA:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_DMA
+/*  Keil::Device:StdPeriph Drivers:EXTI:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_EXTI
+/*  Keil::Device:StdPeriph Drivers:FSMC:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_FSMC
+/*  Keil::Device:StdPeriph Drivers:Flash:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_FLASH
+/*  Keil::Device:StdPeriph Drivers:Framework:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+/*  Keil::Device:StdPeriph Drivers:GPIO:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_GPIO
+/*  Keil::Device:StdPeriph Drivers:I2C:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_I2C
+/*  Keil::Device:StdPeriph Drivers:IWDG:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_IWDG
+/*  Keil::Device:StdPeriph Drivers:PWR:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_PWR
+/*  Keil::Device:StdPeriph Drivers:RCC:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_RCC
+/*  Keil::Device:StdPeriph Drivers:RTC:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_RTC
+/*  Keil::Device:StdPeriph Drivers:TIM:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_TIM
+/*  Keil::Device:StdPeriph Drivers:USART:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_USART
+
+
+#endif /* RTE_COMPONENTS_H */
